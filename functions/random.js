4f9fb3f74ffdbfb061b698dd5057162924dffcc6
Brandon Crooks <crookrb@hotmail.com>
2021-05-21 09:01:28 -0400

    keys test

--- functions/random.js
+++ functions/random.js
@@ -4,6 +4,8 @@ const randomize = (max) => Math.floor(Math.random() * max);
 const getRandomResponse = (data) => data[randomize(data.length)];
 
 exports.handler = async event => {
+  const keys = event.queryStringParameters;
+  console.log('✊🏾 keys', keys);
   return {
     statusCode: 200,
     body: JSON.stringify(getRandomResponse(responses)),
