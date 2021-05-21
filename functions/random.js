9f19dea087e032ea29c076e956b8f2225d736d9f
Brandon Crooks <crookrb@hotmail.com>
2021-05-21 14:17:00 -0400

    refactor random function

--- functions/random.js
+++ functions/random.js
@@ -5,8 +5,39 @@ const getRandomResponse = (data) => data[randomize(data.length)];
 
 exports.handler = async event => {
   const urlParameters = event.queryStringParameters;
-  let responseBody = {};
+  let statusCode = 200;
+  let responseBody;
 
+  try {
+    Object.entries(urlParameters).forEach((param, index) => {
+      let paramKey = param[0];
+      let paramValue = param[1].split(', ');
+
+      let responseKey = Object.keys(responses[0]);
+      let responseValue = responses[0][paramKey];
+
+      if (paramValue.length === 1) {
+        // if only 1 param
+        const result = responses.find((responseValue) => paramValue[0] === responseValue[paramKey]);
+        responseBody.push(result);
+        return null;
+      } else if (paramValue.length > 0) {
+        // if multipe params
+        paramValue.forEach((val, index) => {
+          const result = responses.find((responseValue) => val === responseValue[paramKey]);
+          responseBody.push(result);
+          return null;
+        });
+      } else {
+        statusCode = 422,
+        responseBody.push(JSON.stringify('No params\n'));
+      };
+    })
+  } catch (err) {
+    statusCode = 422,
+    responseBody.push(JSON.stringify('Something went wrong\n'));
+    responseBody.push(JSON.stringify(err));
+  }
   // if (urlParameters) {
   //   urlParameters.forEach((key) => responseBody.push(getRandomResponse(responses)[key]));
   // } else {
@@ -14,7 +45,7 @@ exports.handler = async event => {
   // }
 
   return {
-    statusCode: 200,
-    body: JSON.stringify(urlParameters),
+    statusCode,
+    body: JSON.stringify(responseBody),
   }
 }
