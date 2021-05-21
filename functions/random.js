8329d92f1602ec7e4999a110eb92513d3ef90ab4
Brandon Crooks <crookrb@hotmail.com>
2021-05-21 08:34:51 -0400

    initial commit

---
+++ functions/random.js
@@ -0,0 +1,11 @@
+const responses = require('../data/responses.json');
+
+const randomize = (max) => Math.floor(Math.random() * max);
+const getRandomResponse = (data) => data[randomize(data.length)];
+
+exports.handler = async event => {
+  return {
+    statusCode: 200,
+    body: getRandomResponse(responses),
+  }
+}
